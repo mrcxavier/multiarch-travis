ef81cf0f0c127cc17f6312263ccba0cdd3bdc335
Marcio Xavier <marcioas@br.ibm.com>
2021-08-21 12:54:53 -0300

    removing container dir
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 ARCH=$(uname -m)
 
-docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
-docker tag container/$ARCH quay.io/marcioas/multiarch-travis:$ARCH
+docker build -t $ARCH -f ./$ARCH/Dockerfile .
+docker tag $ARCH quay.io/marcioas/multiarch-travis:$ARCH
 docker login quay.io -u $bot_user -p $bot_pass
 docker push quay.io/marcioas/multiarch-travis:$ARCH
