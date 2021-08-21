a419c5f45152a30e32822667b436fd8a80db2dab
Marcio Xavier <marcioas@br.ibm.com>
2021-08-21 12:51:46 -0300

    fixing uname var
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ARCH=$(uname -a)
+ARCH=$(uname -m)
 
-docker build -t $ARCH -f ./$ARCH/Dockerfile .
-docker tag $ARCH quay.io/marcioas/multiarch-travis:$ARCH
+docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
+docker tag container/$ARCH quay.io/marcioas/multiarch-travis:$ARCH
 docker login quay.io -u $bot_user -p $bot_pass
 docker push quay.io/marcioas/multiarch-travis:$ARCH
