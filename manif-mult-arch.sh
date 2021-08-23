f221f1f73717dd9c3b556ea490e75346a264bd29
Marcio Xavier <marcioas@br.ibm.com>
2021-08-23 11:52:45 -0300

    Manifest
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

---
+++ manif-mult-arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker manifest create quay.io/marcioas/multiarch-travis:latest \
+quay.io/marcioas/multiarch-travis:x86_64 quay.io/marcioas/multiarch-travis:ppc64le
+
+docker manifest inspect quay.io/marcioas/multiarch-travis 
+
+docker login quay.io -u "$bot_user" -p $bot_pass
+
+docker manifest push quay.io/marcioas/multiarch-travis:latest
