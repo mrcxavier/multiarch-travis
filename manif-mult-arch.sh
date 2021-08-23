67e1ff6376db58f674a3f59d0ab762cb70265a4f
Marcio Xavier <marcioas@br.ibm.com>
2021-08-23 15:07:35 -0300

    - adjust manifest
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- manif-mult-arch.sh
+++ manif-mult-arch.sh
@@ -3,9 +3,10 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create quay.io/marcioas/multiarch-travis:latest \
-quay.io/marcioas/multiarch-travis:x86_64 quay.io/marcioas/multiarch-travis:ppc64le
+  quay.io/marcioas/multiarch-travis:x86_64 \ 
+  quay.io/marcioas/multiarch-travis:ppc64le
 
-docker manifest inspect quay.io/marcioas/multiarch-travis 
+docker manifest inspect --verbose quay.io/marcioas/multiarch-travis 
 
 docker login quay.io -u "$bot_user" -p $bot_pass
 
