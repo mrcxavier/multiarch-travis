2becf88775ac78bc935c462eba9a7823271580db
Marcio Xavier <marcioas@br.ibm.com>
2021-08-24 19:41:06 -0300

    - remove directory
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 ARCH=$(uname -m)
 
 docker build -t $ARCH -f ./Dockerfile .
-docker tag directory/$ARCH quay.io/marcioas/multiarch-travis:$ARCH
+docker tag $ARCH quay.io/marcioas/multiarch-travis:$ARCH
 docker login quay.io -u $bot_user -p $bot_pass
 docker push quay.io/marcioas/multiarch-travis:$ARCH
