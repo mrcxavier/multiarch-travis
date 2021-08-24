4658b390a8fa4c88bc19b56a710d3b3baee5835e
Marcio Xavier <marcioas@br.ibm.com>
2021-08-24 13:33:08 -0300

    - buildah test1
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- manif-mult-arch.sh
+++ manif-mult-arch.sh
@@ -3,10 +3,14 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create quay.io/marcioas/multiarch-travis:v3 \
-quay.io/marcioas/multiarch-travis:x86_64 \ 
-quay.io/marcioas/multiarch-travis:ppc64le
 
-docker manifest inspect --verbose quay.io/marcioas/multiarch-travis 
+buildah manifest add --override-arch=x86_84 --os=linux --arch=x86_64 quay.io/marcioas/multiarch-travis:v2 quay.io/marcioas/multiarch-travis:latest
+
+buildah manifest add --override-arch=ppc64le --os=linux --arch=ppc64le quay.io/marcioas/multiarch-travis:v2 quay.io/marcioas/multiarch-travis:latest
+#quay.io/marcioas/multiarch-travis:x86_64 \ 
+#quay.io/marcioas/multiarch-travis:ppc64le
+
+docker manifest inspect  quay.io/marcioas/multiarch-travis:v2
 
 docker login quay.io -u "$bot_user" -p $bot_pass
 
