0e3f3220612fa06e5298b2d13930a4bdd4e42686
Marcio Xavier <marcioas@br.ibm.com>
2021-08-24 11:34:10 -0300

    - docker manifest back
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- manif-mult-arch.sh
+++ manif-mult-arch.sh
@@ -3,10 +3,8 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create quay.io/marcioas/multiarch-travis:latest 
-buildah manifest add --override-arch=ppc64le --override-os=linux  --os=linux --arch=x86_64 quay.io/marcioas/multiarch-travis:v2 quay.io/marcioas/multiarch-travis:latest
-buildah manifest add --override-arch=ppc64le --override-os=linux  --os=linux --arch=ppc64le quay.io/marcioas/multiarch-travis:v2 quay.io/marcioas/multiarch-travis:latest
-#quay.io/marcioas/multiarch-travis:x86_64 \ 
-#quay.io/marcioas/multiarch-travis:ppc64le
+quay.io/marcioas/multiarch-travis:x86_64 \ 
+quay.io/marcioas/multiarch-travis:ppc64le
 
 docker manifest inspect --verbose quay.io/marcioas/multiarch-travis 
 
