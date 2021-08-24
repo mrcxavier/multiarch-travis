40de6c093356825ff2f6c0be9b3e2e868efdd340
Marcio Xavier <marcioas@br.ibm.com>
2021-08-24 19:33:56 -0300

    - fixing docker build
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 ARCH=$(uname -m)
 
-docker build $ARCH -t directory/$ARCH -f ./Dockerfile .
+docker build $ARCH -t $ARCH -f ./Dockerfile .
 docker tag directory/$ARCH quay.io/marcioas/multiarch-travis:$ARCH
 docker login quay.io -u $bot_user -p $bot_pass
 docker push quay.io/marcioas/multiarch-travis:$ARCH
