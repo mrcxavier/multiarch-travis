ddb15ca0f204ec0f5044f719a6f50ef269109b2f
Marcio Xavier <marcioas@br.ibm.com>
2021-08-24 20:15:43 -0300

    - add different version
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- manif-mult-arch.sh
+++ manif-mult-arch.sh
@@ -2,12 +2,12 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/marcioas/multiarch-travis:latest \
+docker manifest create quay.io/marcioas/multiarch-travis:version1 \
 --amend 'quay.io/marcioas/multiarch-travis:x86_64' \
 --amend 'quay.io/marcioas/multiarch-travis:ppc64le'
 
-docker manifest inspect  quay.io/marcioas/multiarch-travis
+docker manifest inspect  quay.io/marcioas/multiarch-travis:version1
 
 docker login quay.io -u "$bot_user" -p $bot_pass
 
-docker manifest push quay.io/marcioas/multiarch-travis:latest
+docker manifest push quay.io/marcioas/multiarch-travis:version1
