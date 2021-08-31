45bf7672d4e8a38e90f2fbcb71556ef774c677dd
Marcio Xavier <marcioas@br.ibm.com>
2021-08-31 10:35:22 -0300

    - Manifest
    Signed-off-by: Marcio Xavier <marcioas@br.ibm.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:18.04 
-RUN apt-get update
-RUN apt-get install squid -y
-RUN touch HELLO-WORLD
+#RUN apt-get update
+#RUN apt-get install squid -y
+#RUN touch HELLO-WORLD
--- manif-mult-arch.sh
+++ manif-mult-arch.sh
@@ -2,12 +2,12 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/marcioas/multiarch-travis:version1 \
---amend 'quay.io/marcioas/multiarch-travis:x86_64' \
---amend 'quay.io/marcioas/multiarch-travis:ppc64le'
+docker manifest create "quay.io/marcioas/multiarch-travis:my-image" \
+'quay.io/marcioas/multiarch-travis:x86_64' \
+'quay.io/marcioas/multiarch-travis:ppc64le'
 
-docker manifest inspect  quay.io/marcioas/multiarch-travis:version1
+docker manifest inspect  quay.io/marcioas/multiarch-travis:my-image
 
 docker login quay.io -u "$bot_user" -p $bot_pass
 
-docker manifest push quay.io/marcioas/multiarch-travis:version1
+docker manifest push quay.io/marcioas/multiarch-travis:my-image
